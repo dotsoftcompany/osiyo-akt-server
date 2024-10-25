9dd06780d77af119314e954241c21bebd78f6982
Mukhammadxuja Anvarov <87266381+mukhammadxuja@users.noreply.github.com>
2024-10-25 15:15:22 +0500

    demo teacher server

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 const admin = require("firebase-admin");
-const credentails = require("./serviceAccountKey.json");
+const credentials = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(credentials),
+});
 
 app.use((req, res, next) => {
   res.header(
@@ -19,27 +22,22 @@ app.use((req, res, next) => {
   next();
 });
 
-admin.initializeApp({
-  credential: admin.credential.cert(credentails),
-});
-
 app.post("/add-teacher", async (req, res) => {
   try {
     const user = {
       email: req.body.email,
       password: req.body.password,
-      name: req.body.name,
-      surname: req.body.surname,
-      phoneNumber: req.body.phoneNumber,
+      fullName: req.body.fullName,
+      phone: req.body.phone,
       address: req.body.address,
-      freeUseEndDate: req.body.freeUseEndDate,
-      timestamp: req.body.timestamp,
-      uid: req.body.uid,
-      photoUrl: req.body.photoUrl,
-      status: req.body.status,
-      role: req.body.role,
-      sellPercent: req.body.sellPercent,
+      position: req.body.position,
+      roles: req.body.role,
+      isTeacherUpdate: req.body.isTeacherUpdate,
     };
+
+    if (!email || !email.includes("@")) {
+      return res.status(400).json({ message: "Invalid email format." });
+    }
 
     const userResponse = await admin.auth().createUser({
       email: user.email,
@@ -49,35 +47,28 @@ app.post("/add-teacher", async (req, res) => {
     });
 
     await admin
+      .auth()
+      .setCustomUserClaims(userResponse.uid, { role: "teacher" });
+
+    await admin
       .firestore()
       .collection("teachers")
-      .doc(`${userResponse.uid}`)
+      .doc(userResponse.uid)
       .set({
-        name: user.name,
-        surname: user.surname,
-        email: user.email,
-        address: user.address,
-        phone: user.phoneNumber,
-        curr: "USD",
+        ...user,
         timestamp: new Date(),
-        prefixtime: new Date().getTime(),
-        freeUseEndDate: user.freeUseEndDate,
-        payment: true,
         id: userResponse.uid,
-        password: user.password,
-        manageruid: user.uid,
-        photoUrl: user.photoUrl,
-        role: user.role,
-        status: user.status,
-        sellPercent: user.sellPercent,
       });
-    res
-      .status(201)
-      .json({ message: "User registered successfully", uid: userResponse.uid });
+
+    res.status(201).json({
+      message: "Teacher registered successfully",
+      uid: userResponse.uid,
+    });
+    console.log("Teacher added successfully", res.status);
   } catch (error) {
     res
       .status(400)
-      .json({ message: "Error registering user", error: error.message });
+      .json({ message: "Error registering teacher", error: error.message });
     console.log(error);
   }
 });
@@ -85,5 +76,5 @@ app.post("/add-teacher", async (req, res) => {
 const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log("Server is running");
+  console.log(`Server is running on port ${PORT}`);
 });
